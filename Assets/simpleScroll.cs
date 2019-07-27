6aad69ed37b7da592c43fd781678230c3f27c559
Bethany Drake <bethanyd@thoughtworks.com>
2019-07-27 19:54:03 +1000

    jump over obstical

---
+++ Assets/simpleScroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleScroll : MonoBehaviour
+{
+
+    public float speed = 1;
+    public float maxLeft;
+    public float maxRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+     void Update()
+    {
+        if (transform.position.x < maxLeft) {
+            transform.position = new Vector3(maxRight, transform.position.y, 0);
+        }
+
+
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+
+    }
+}
