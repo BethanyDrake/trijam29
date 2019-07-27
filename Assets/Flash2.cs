225610f0ece5116290d310448185ca6c04617e94
Bethany Drake <bethanyd@thoughtworks.com>
2019-07-27 20:40:44 +1000

    flash on hit

---
+++ Assets/Flash2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flash2 : MonoBehaviour
+{
+
+
+    private Color oldColor;
+    private bool flashing = false;
+    private SpriteRenderer sr;
+
+    public float flashTime = 0.2f;
+    private float timeRemaining;
+
+    public void flash() {
+        Debug.Log("flashing");
+        oldColor = sr.color;
+        sr.color = Color.red;
+        timeRemaining = flashTime;
+        flashing = true;
+    }
+    // Start is called before the first frame update
+    void Start() {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flashing) {
+            Debug.Log(Time.deltaTime);
+            timeRemaining -= Time.deltaTime;
+            Debug.Log(timeRemaining);
+        }
+
+        if (flashing && timeRemaining < 0) {
+            flashing = false;
+            GetComponent<SpriteRenderer>().color = oldColor;;
+        }
+
+    }
+}
